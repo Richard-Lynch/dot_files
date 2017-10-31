8a22e89ef8c1a8a5b2fbf3189807eabd826ef10d
Richard Lynch <rich753@hotmail.com>
2017-10-31 11:28:27 +0000

    fixed linux sed

--- .shell/whichEditorLinux.sh
+++ .shell/whichEditorLinux.sh
@@ -16,7 +16,7 @@ makeLocal () {
         if [ \! -s $1 ] ; then echo "" >> $1 ; echo "added blank line to file" ;  fi ;
         # insert the correct fileString to the top of the file
         echo "adding line"
-        gsed -i "1i#!$newExeName" $1 # gsed = gnu-sed ( homebrew! )
+        sed -i "1i#!$newExeName" $1 # gsed = gnu-sed ( homebrew! )
     fi
 }
 
