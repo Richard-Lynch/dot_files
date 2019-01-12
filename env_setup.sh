ac791bbf795b26bcb7926a1c74967c9954d6593c
Richard Lynch <rich753@hotmail.com>
2019-01-12 13:58:39 +0000

    Disable env_setup script until better solution is decided on

--- env_setup.sh
+++ env_setup.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+# This script is currently under consideration, it may be removed completely in favor of several other alternative options.
+# Option 1: Manual Copying/Linking -> safe but slow and annoying
+# Option 2: Automatic linking of minimum files, and ensure everything else references these files
+# Option 3 (best): Create a script that only cats the needed lines into the existing bashrc/bash_profile to source these files correctly
+
+echo "please read comments at top of this file"
+exit -1
+
 unameOut="$(uname -s)"
 case "${unameOut}" in
     Linux*)     machine=Linux;;
@@ -10,130 +18,73 @@ case "${unameOut}" in
 esac
 echo "machine: $machine"
 
-# exit 
+# -- Check if git is installed
+# if [ ! -x /usr/bin/git ] ; then
+#     echo "Error: Git not installed.\nPlease install git and run again.";
+#     exit 1;
+# fi
 
-if [ ! -x /usr/bin/git ] ; then
-    echo "Error: Git not installed.\nPlease install git and run again.";
-    exit 1;
-fi
+# -- Options to set locations etc
+# while getops ":d:b:f:" opt; do
+#     case $opt in
+#         :)
+#             echo "option -$OPTARG requires arg" >&2
+#             ;;
+#         d)
+#             echo "DOTFILES being set to '$DOTFILES'"
+#             DOTFILES=$OPTARG
+#             ;;
+#         b)
+#             echo "BACKUP being set to '$OPTARG'"
+#             BACKUP=$OPTARG
+#             ;;
+#         i)
+#             echo "ignore filenames being set from file '$OPTARG'"
+#             filenames=$(cat $OPTARG)
+#             echo "filenames set to '$filenames'"
+#             ;;
+#         h)
+#             echo "home being set to '$OPTARG'"
+#             HOMEdir=$OPTARG
+#             ;;
+#         /?)
+#             echo "Invalid Flag '-$OPTARG'"
+#             exit 1
+#             ;;
+#     esac
+# done
 
-while getops ":d:b:f:" opt; do
-    case $opt in
-        :)
-            echo "option -$OPTARG requires arg" >&2
-            ;;
-        d)
-            echo "DOTFILES being set to '$DOTFILES'"
-            DOTFILES=$OPTARG
-            ;;
-        b)
-            echo "BACKUP being set to '$OPTARG'"
-            BACKUP=$OPTARG
-            ;;
-        i)
-            echo "ignore filenames being set from file '$OPTARG'"
-            filenames=$(cat $OPTARG)
-            echo "filenames set to '$filenames'"
-            ;;
-        h)
-            echo "home being set to '$OPTARG'"
-            HOMEdir=$OPTARG
-            ;;
-        /?)
-            echo "Invalid Flag '-$OPTARG'"
-            exit 1
-            ;;
-    esac
-done
-
-
-
-
-# TODO option here to set home
+# TODO respect the above options
 HOMEdir=$HOME
-DOTFILES="$HOMEdir/dot_files/"
-# TODO option here to set backup dir
-BACKUP="$HOMEdir/Dot_file_backup_$(date +%Y-%m-%d_%H:%M:%S)/";
-# mkdir $BACKUP;
-# TODO option here to set ignore files
+DOTFILES="$(pwd)"
+BACKUP="$DOTFILES/dot_file_backup_$(date +%Y-%m-%d_%H:%M:%S)/";
+# TODO respect ignore files
+
+FILENAMES="$(cd $(pwd) > /dev/null; \ls -A | grep -v --file=.script_ignore )" # mac
+
 echo "DOTFILES: $DOTFILES"
 echo "BACKUP: $BACKUP"
-echo "HOMENAME: $HOMENAME"
+echo "HOMEdir: $HOMEdir"
 echo "FILENAMES: $FILENAMES"
+
+
+
+exit
+
 # ---- Main Copy ----
-# git clone https://github.com/Richard-Lynch/dot_files.git
+# mkdir $BACKUP;
+# for file in $FILENAMES; do
+#     newFile="$DOTFILES/$file"
+#     oldFile="$HOME/$file"
+#     if [ -w "$oldFile" ] ; then # || [ -d "$oldFile" ] ; then
+#         echo "Making backup of \"$oldFile\" in \"$newFile\""
+#         mv $oldFile $BACKUP
+#     else
+#         echo "file \"$oldFile\" doesnt exist"
+#     fi
+#     echo "linking \"$newFile\" to \"$oldFile\""
+#     # TODO should probably be git mv so that the files are still tracked?
+#     ln -s $newFile $oldFile
+# done
 
-# FILENAMES="$(cd ; cd  dot_files/ ; \ls -AI .git)" # linux
-FILENAMES="$(cd ; cd  dot_files/ ; \ls -A | grep -v '.git')" # mac
-echo $FILENAMES
-mkdir $BACKUP;
-for file in $FILENAMES; do
-    newFile="$DOTFILES$file"
-    oldFile="$HOME/$file"
-    if [ -w "$oldFile" ] ; then # || [ -d "$oldFile" ] ; then
-        echo "Making backup of \"$oldFile\" in \"$newFile\""
-        mv $oldFile $BACKUP
-    else
-        echo "file \"$oldFile\" doesnt exist"
-    fi
-    echo "Copying \"$newFile\" to \"$oldFile\""
-    # TODO should probably be git mv so that the files are still tracked?
-    cp -r $newFile $oldFile
-done
-
-# ---- Vim ----
-echo "doing vim"
-ln -s ~/.vim/vimrc ./.vimrc
-mkdir -p ~/.vim/.backups ; 
-mkdir -p ~/.vim/.swaps ; 
-mkdir -p ~/.vim/.undo ; 
-cd .vim ;
-# clone vundle
-mkdir ./bundle
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim ;
-vim +PluginInstall +qall ; 
-#edit showMarks 
-showMarksFile="$HOME/.vim/bundle/ShowMarks/plugin/showmarks.vim" ;
-if [ -f $showMarksFile ] ; then
-    orig="exe 'sign place '.id.' name=ShowMark'.nm.' line='.ln.' buffer='.winbufnr(0)"
-    newl="\
-\" changed by Richard Lynch edits file \n\
-if ln > 0 \n\
-    exe 'sign place '.id.' name=ShowMark'.nm.' line='.ln.' buffer='.winbufnr(0)\n\
-endif\n\
-\" end of change  by Richard Lynch\
-"
-
-    sed -e "s/$orig/$newl/" $showMarksFile > ./tempChangeLineFile ;
-    cp $showMarksFile $showMarksFile.bac_$(date +%Y-%m-%d_%H:%M:%S) ; 
-    mv ./tempChangeLineFile $showMarksFile ;
-else
-    echo "ShowMarks file not found!"
-    echo $showMarksFile
-fi
-
-# youcompleteme
-case "${unameOut}" in
-    Linux*)     
-        machine=Linux
-        sudo apt-get install build-essential cmake
-        sudo apt-get install python-dev python3-dev
-        ;;
-    Darwin*)    
-        machine=Mac
-        brew install python
-        brew install python3
-        brew install cmake
-        ;;
-    *)          
-        echo "unknown machine, not compiling ycm"
-        exit
-        ;;
-esac
-
-cd ~/.vim/bundle/YouCompleteMe
-./install.py --clang-completer
-
-
-mv $DOTFILES/.git $HOMEdir/
-# git add $FILENAMES
+# ---- Special Copy --- 
